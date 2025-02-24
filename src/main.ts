97fcacf2dda67d782f7e39dcf0346409568b29b7
Luca Scalzotto <luca@scalzotto.nl>
2025-02-24 15:16:57 +0100

    JSON highlighting

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,24 @@ import './style.scss';
 import Quill from 'quill';
 import 'quill/dist/quill.core.css';
 import 'quill/dist/quill.snow.css';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/default.min.css';
+import json from 'highlight.js/lib/languages/json';
+
+hljs.registerLanguage('json', json);
 
 const quill = new Quill('#editor', {
   theme: 'snow',
 });
 
-const delta = document.getElementById('delta')!;
+const deltaOutput = document.getElementById('delta')!;
 
-quill.on('text-change', () => {
-  delta.textContent = JSON.stringify(quill.getContents(), null, 2);
-});
+update();
+quill.on('text-change', update);
+
+function update(): void {
+  deltaOutput.textContent = JSON.stringify(quill.getContents(), null, 2);
+
+  deltaOutput.removeAttribute('data-highlighted');
+  hljs.highlightElement(deltaOutput);
+}
