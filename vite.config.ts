52720622093bf029413ac878561942fba16022d8
Luca Scalzotto <luca@scalzotto.nl>
2025-02-25 21:02:03 +0100

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'web',
-  base: 'quill-delta-to-html',
+  base: '/quill-delta-to-html',
   build: {
     outDir: '../pages',
   },
