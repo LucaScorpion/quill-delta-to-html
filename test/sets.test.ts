0b0d4579789df2b38ac77a2db391571da6d9402e
Luca Scalzotto <luca@scalzotto.nl>
2025-02-25 10:54:54 +0100

    Setup testing

---
+++ test/sets.test.ts
@@ -0,0 +1,24 @@
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import { deltaToHtml } from '../lib';
+import { Delta } from '../lib/delta';
+
+describe('sets', () => {
+  const setsDir = path.resolve(__dirname, 'sets');
+  const names = fs
+    .readdirSync(setsDir)
+    .filter((f) => f.endsWith('.json'))
+    .map((f) => f.substring(0, f.length - 5));
+
+  it.each(names)('generates the correct html from delta: %s', (name) => {
+    const input: Delta = JSON.parse(
+      fs.readFileSync(path.resolve(setsDir, `${name}.json`)).toString(),
+    );
+    const expected = fs
+      .readFileSync(path.resolve(setsDir, `${name}.html`))
+      .toString()
+      .trim();
+
+    expect(deltaToHtml(input)).toBe(expected);
+  });
+});
